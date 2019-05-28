57535ed3d577def519a85d617b2850e049640add
cesar20340 <50148032+cesar20340@users.noreply.github.com>
2019-05-28 10:02:49 -0400

    Avance control de inventario

---
+++ ferreteria/app/Http/Controllers/controller1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ControlDeInventario\Http\Controllers;
+
+use ControlDeInventario\Http\Controllers\Controller;
+
+class controller1 extends Controller {
+    public function prueba(){
+        return 'Estoy dentro de controller1';
+    }
+
+}
